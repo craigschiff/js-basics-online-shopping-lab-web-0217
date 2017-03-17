61e20c3d15236a3ce6c617a2a48ce75bc0ccfa43
github username <github email address>
2017-03-17 12:38:10 -0400

    close enough

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,64 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 100)
+  cart.push({ [item] : price})
+  console.log(`${item} has been added to your cart.`)
+  setCart(cart)
+  return getCart()
+}
+function viewCart(){
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  }else{
+    console.log("In your cart, you have ")
+    for (let i = 0; i < cart.length; i +=1){
+      for (let item in cart[i]){
+      console.log(`${item} at $${cart[i][item]}`)
+    }
+    }
+  }
+
+
+}
+
+
+function removeFromCart(item){
+  // for (let i = 0; i < cart.length; i +=1){
+    // if (Object.keys(cart).includes(item)){
+      // let index = Object.keys(cart).indexOf(item)
+      // cart
+      for (let i = 0; i <cart.length; i+=1){
+        if (Object.keys(cart[i])[0] === `${item}`){
+          delete cart[i]
+          const printCart = cart.map((item) => {
+            let key = Object.keys[item][0]
+            let price = Object.keys[item][0]
+            return `${key} at ${price}`
+
+          })
+          console.log(`In your cart you have ${printCart.join(", ")}.`)
+        }
+  // else {
+  //   console.log("That item is not in your cart.")
+  // }
+}
+  console.log("That item is not in your cart.")
+
+}
+
+function placeOrder (card) {
+  if (card === undefined){
+    console.log("We don't have a credit card on file for you to place your order.")
+}else {
+  `Your total cost is $${total()}, which will be charged to the card ${card}.`
+}
+cart.length = 0
 }
